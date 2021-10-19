96a8299705e75778927a8edd7f4ff9c7650f45c9
Tran Trung <quoctrung.tran210@gmail.com>
2021-10-20 00:58:42 +0700

    list categories and add/edit/delete

--- resources/views/admincp/category/create.blade.php
+++ resources/views/admincp/category/create.blade.php
@@ -30,11 +30,11 @@
                             @method('post')
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Tên Danh Mục</label>
-                                <input type="text" class="form-control" name="category_name">
+                                <input type="text" class="form-control" name="category_name" value="{{ old('category_name') }}">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Mô Tả Danh Mục</label>
-                                <input type="text" class="form-control" name="description">
+                                <input type="text" class="form-control" name="description" value="{{ old('description') }}">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Trạng Thái Danh Mục</label>
