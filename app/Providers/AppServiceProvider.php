9cd3a84df417986e46cc79feb9b11092ecdbdb69
Tran Trung <quoctrung.tran210@gmail.com>
2021-11-08 19:03:18 +0700

    initial release5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\Models\Category;
 use Carbon\Carbon;
 
